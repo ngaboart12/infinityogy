2e42774848fd147e8aac8f08cc7bbf884b9be5a0
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-06-03 20:14:51 +0200

    Training Program

--- components/events/JobModal.jsx
+++ components/events/JobModal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRef, useEffect } from "react";
 import "./jobModal.css";
+import Link from "next/link";
 
 const JobModal = ({ isOpen, onClose, jobDetails }) => {
   const modalRef = useRef();
@@ -35,33 +36,74 @@ const JobModal = ({ isOpen, onClose, jobDetails }) => {
         <div className=" absolute top-[-6vh] py-4 w-[100px] flex items-center justify-center  bg-orange-400 rounded-md">
           <h1 className="text-[30px] text-white">
             {jobDetails.jobName.substring(0, 2).toUpperCase()}
+            {/* Professional Software Development Training Program */}
           </h1>
         </div>
         <div className="disc py-14">
           <div className=" items-center flex flex-col">
-            <h2 className="flex  flex-row items-center gap-1 mt-4">
-              <span className="text-black">Job Titel:</span>
-              {jobDetails.jobName}
+            <h2 className="flex text-black  flex-row items-center gap-1 mt-4">
+              <span className="text-orange-400">Job Title:</span>
+              {/* {jobDetails.jobName} */}
+              Professional Software Development Training Program
             </h2>
             <p className="text-black">
-              Salary:{" "}
-              <span className="text-orange-400 ">{jobDetails.jobSalary}</span>{" "}
-              Frw
+              Cost of Attendance:{" "}
+              <span className="text-orange-400 ">50,000</span> Frws
             </p>
-            <p className="text-blac">
-              Time:{" "}
-              <span className="text-orange-400">{jobDetails.jobTime}</span>
+            <p className="text-black">
+              Time: <span className="text-orange-400">Flexible time</span>
             </p>
           </div>
 
           <p className="text-start text-black px-5">
-            {jobDetails.jobDescription}
+            This is Professional IT, Training Program, designed by Infinity Tech
+            Solution Ltd in partnership with GGO. Our one-month program is
+            perfect for secondary school students, university students, recent
+            graduates, and anyone interested in IT. The training costs 50,000
+            FRW as mentioned above, payable in installments, and covers various
+            tech stacks to ensure you gain the latest skills in the field. This
+            partial training program offers flexibility and high-quality
+            education. Join us and take the next step in your IT career!
+            <ul className="p-2 text-black/50">
+              <li>
+                &bull;JavaScript (React JS, Express, Node.js) with emphasis on Load
+                balancing and Microservices architecture.
+              </li>
+              <li>
+              &bull;Python (Django, Flask) with a focus on Load balancing and
+                Microservices implementation.
+              </li>
+              <li>
+              &bull;Cloud services like AWS, with an emphasis on Load balancing and
+                Microservices deployment.
+              </li>
+              <li>
+              &bull;Database administration, including SQL and NoSQL databases, with
+                a focus on Load balancing and Microservices integration.
+              </li>
+              <li>
+                {" "}
+                &bull;Advanced integration solutions for payment gateways and security
+                protocols.
+              </li>
+              <li>&bull;UI & UX design principles.</li>
+            </ul>
+            Our goal is to provide a professional and enriching learning
+            experience that prepares you for success in the industry. Whether
+            you're a secondary school graduate, university student, or working
+            professional, our training program is tailored to meet your needs.
+            Please take a moment to fill out the registration form below to
+            secure your spot in our upcoming training session. Rest assured; the
+            form will only take you 2 minutes to complete. We eagerly anticipate
+            your participation in our program.
           </p>
           {/* Add other job details as needed */}
         </div>
-        <button className="rounded-md border p-4 hover:bg-orange-400/80 bg-orange-400 text-white w-[200px]">
-          Apply now
-        </button>
+        <Link href="https://forms.gle/nsgsBKmMQasVzzuGA" target="_blank">
+          <button className="rounded-md border p-4 hover:bg-orange-400/80 bg-orange-400 text-white w-[200px]">
+            Apply now
+          </button>
+        </Link>
       </div>
     </div>
   );
